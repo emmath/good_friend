edd745d297f1a4bb4df1dc998fcc976f158a0d13
emmath <emmatrevena@gmail.com>
2013-07-29 09:17:29 -0700

    Generate the Good Friend app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodFriend::Application.config.secret_token = '62d661a5d4a640a77bca19e21ec923e537c87a23d240066d331cf645c1efdce3f1d73319e247b71c8111e1fb6fc2990349c9cb5772cd8604b4e8c55883215ade'
